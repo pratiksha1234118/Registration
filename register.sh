6faf8b84a233fd7028fcc7f3b3045e85dd7d1387
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-11 17:46:54 +0530

    Added: first name is added

--- register.sh
+++ register.sh
@@ -1,2 +1,15 @@
 echo "user registration"
+#firstname
+#!/bin/bash -x
+echo "Enter your first name"
+read FName;
+pattern="^[A-Z]{1}[a-z]{2,}$";
+if [[ $FName =~ $pattern ]]
+then
+        echo "First name is valid";
+else
+        echo "First name is invalid";
+fi
+
+
 
