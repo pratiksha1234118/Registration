46eec9812669dd29b3f95024259eec6c971d6c23
pratiksha1234118 <pratikshagavande1699@gmail.com>
2020-07-11 17:59:25 +0530

    Added: Email is added

--- register.sh
+++ register.sh
@@ -24,3 +24,13 @@ else
         echo "last name is invalid";
 fi
 
+#email
+echo "Enter your email id"
+read Email;
+pattern1="^[0-9a-zA-z]+([.][0-9A-za-z]+)*@[a-z]+.[A-Za-z]{2,4}([.][a-z]{2})*$";
+if [[ $Email =~ $pattern1 ]]
+then
+        echo "entered email is valid";
+else
+        echo "entered email ids invalid";
+fi
